87c514d5fb9565e308c05a9a1022dc656886d8bc
andreasoie <oeie.andreas@gmail.com>
2022-07-06 12:17:32 +0200

    removed api func from common api

--- synthetic_data/common/api.py
+++ synthetic_data/common/api.py
@@ -1,11 +1,10 @@
-import json
-
 import numpy as np
 import pandas as pd
 import requests
-from common.config import Config
 
-cfg = Config()
+from synthetic_data.common.config import LocalConfig
+
+cfg = LocalConfig()
 
 
 def save_time_series(name: str, ts: pd.DataFrame, params: dict) -> dict:
@@ -15,8 +14,7 @@ def save_time_series(name: str, ts: pd.DataFrame, params: dict) -> dict:
         "y": ts["y"].tolist(),
         "parameters": params,
     }
-
-    ENDPOINT = cfg.API_URI + "/datasets"
+    ENDPOINT = cfg.API_URI + "/dataset"
     response = requests.post(ENDPOINT, json=payload)
     return response.json()
 
@@ -25,20 +23,6 @@ def get_all_time_series(limit: int = 100) -> list:
     ENDPOINT = cfg.API_URI + "/datasets"
     response = requests.get(ENDPOINT, params={"limit": limit})
     return response.json()
-
-
-def get_time_series(name: str) -> dict:
-    ENDPOINT = cfg.API_URI + "/datasets"
-    response = requests.get(ENDPOINT, params={"name": name})
-    response = response.json()
-    return response.json()
-
-
-def load_time_series_as_numpy(name: str) -> np.ndarray:
-    response = get_time_series(name=name)
-    dataset = response["dataset"]
-    dataset = np.array(dataset["y"])  # only need the y-value (?)
-    return dataset.reshape(1, -1)  # returns with shape (1, N)
 
 
 def get_model_meta():
