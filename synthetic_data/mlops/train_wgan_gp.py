c40cdf65e26c0952108d2411739418ddd38b0bd2
andreasoie <oeie.andreas@gmail.com>
2022-08-02 16:56:39 +0200

    added bokeh vizualization to wgan-gp training

--- synthetic_data/mlops/train_wgan_gp.py
+++ synthetic_data/mlops/train_wgan_gp.py
@@ -6,6 +6,9 @@ import mlflow
 import numpy as np
 import ray
 import torch
+from bokeh.io import output_file, save
+from bokeh.layouts import gridplot
+from bokeh.plotting import figure
 from ray import tune
 from ray.tune.integration.mlflow import MLflowLoggerCallback
 from torch.autograd import grad as torch_grad
@@ -29,6 +32,7 @@ class Trainer:
         critic_iterations: int = 5,
         print_every: int = 200,
         checkpoint_frequency: int = 200,
+        plotmode: str = "plt",  # "plt" | "bokeh"
     ):
         self.generator = generator
         self.critic = critic
@@ -42,6 +46,20 @@ class Trainer:
         self.print_every = print_every
         self.checkpoint_frequency = checkpoint_frequency
         self.device = get_device()
+        self.plotmode = plotmode
+
+        if self.plotmode == "plt":
+            self.fixdir = os.path.join("outputs", "fixed")
+            self.dyndir = os.path.join("outputs", "dynamic")
+            os.makedirs(self.fixdir, exist_ok=True)
+            os.makedirs(self.dyndir, exist_ok=True)
+        elif self.plotmode == "bokeh":
+            outdir = os.path.join("outputs")
+            os.makedirs(outdir, exist_ok=True)
+            output_file(f"{outdir}/epochs.html")
+            self.bokeh_figures = []
+        else:
+            raise ValueError(f"Unknown plotmode {self.plotmode}")
 
     def train_critic(self, real_data):
         batch_size = real_data.size(0)
@@ -155,14 +173,15 @@ class Trainer:
             if epoch % self.print_every == 0:
                 # Sample a different region of the latent distribution to check for mode collapse
                 dynamic_latents = torch.randn(noise_shape, device=self.device)
-                self.eval_and_savefig(epoch, fixed_latents, dynamic_latents)
+
+                if self.plotmode == "plt":
+                    self.eval_and_savefig_plt(epoch, fixed_latents, dynamic_latents)
+                elif self.plotmode == "bokeh":
+                    self.eval_and_savefig_bokeh(epoch, fixed_latents, dynamic_latents)
+        save(gridplot(self.bokeh_figures, ncols=2))
 
     @torch.no_grad()
-    def eval_and_savefig(self, epoch, fixd_latent, dyn_latent):
-        fixdir = os.path.join("outputs", "fixed")
-        dyndir = os.path.join("outputs", "dynamic")
-        os.makedirs(fixdir, exist_ok=True)
-        os.makedirs(dyndir, exist_ok=True)
+    def eval_and_savefig_plt(self, epoch, fixd_latent, dyn_latent):
 
         self.generator.eval()
         # Generate fake data using both fixed and dynamic latents
@@ -171,13 +190,73 @@ class Trainer:
 
         plt.figure()
         plt.plot(fake_data_fixed_latents.numpy()[0].T)
-        plt.savefig(f"{fixdir}/epoch_{epoch}.png")
+        plt.savefig(f"{self.fixdir}/epoch_{epoch}.png")
         plt.close()
 
         plt.figure()
         plt.plot(fake_data_dynamic_latents.numpy()[0].T)
-        plt.savefig(f"{dyndir}/epoch_{epoch}.png")
+        plt.savefig(f"{self.dyndir}/epoch_{epoch}.png")
         plt.close()
+
+        self.generator.train()
+
+    @torch.no_grad()
+    def eval_and_savefig_bokeh(self, epoch, fixd_latent, dyn_latent):
+
+        self.generator.eval()
+        # Generate fake data using both fixed and dynamic latents
+        fake_data_fixed_latents = self.generator(fixd_latent).cpu().data
+        fake_data_dynamic_latents = self.generator(dyn_latent).cpu().data
+
+        y_fix_latents = fake_data_fixed_latents.numpy()[0].T
+        x_fix_latents = range(len(y_fix_latents))
+
+        y_dyn_latents = fake_data_dynamic_latents.numpy()[0].T
+        x_dyn_latents = range(len(y_dyn_latents))
+
+        fig = figure(
+            title=f"Epoch {epoch}",
+            x_axis_label="Time step",
+            y_axis_label="Amplitude",
+            aspect_ratio=16 / 9,
+        )
+        fig.line(x_fix_latents, y_fix_latents, legend_label="Fixed", line_width=2)
+        fig.circle(
+            x_fix_latents,
+            y_fix_latents,
+            legend_label="Fixed",
+            line_width=2,
+            fill_color="blue",
+            size=5,
+            alpha=0.75,
+        )
+        self.bokeh_figures.append(fig)
+
+        fig = figure(
+            title=f"Epoch {epoch}",
+            x_axis_label="Time steps",
+            y_axis_label="Amplitude",
+            aspect_ratio=16 / 9,
+        )
+        fig.line(
+            x_dyn_latents,
+            y_dyn_latents,
+            legend_label="Dynamic",
+            line_width=2,
+            color="orangered",
+        )
+        fig.circle(
+            x_dyn_latents,
+            y_dyn_latents,
+            legend_label="Dynamic",
+            line_width=2,
+            fill_color="orangered",
+            color="orangered",
+            size=5,
+            alpha=0.75,
+        )
+        self.bokeh_figures.append(fig)
+
         self.generator.train()
 
     def save_statedict(self, epoch):
@@ -209,13 +288,13 @@ def run_training_session(config, dataset=None):
 
     device = get_device()
 
-    SPLIT_SIZE = 100
+    SPLIT_SIZE = 50
     SPLIT_RATIO = 1
     DATASET_NAME = "AMP20"
 
     dataset = HarmonicDataset(DATASET_NAME, SPLIT_SIZE, SPLIT_RATIO)
     dataloader = DataLoader(
-        dataset, batch_size=config["batch_size"], num_workers=8, pin_memory=True
+        dataset, batch_size=config["batch_size"], num_workers=4, pin_memory=True
     )
 
     seq_length = dataset.dataset.shape[1]
@@ -229,7 +308,9 @@ def run_training_session(config, dataset=None):
     optimG = torch.optim.RMSprop(generator.parameters(), lr=config["lr"])
     optimC = torch.optim.RMSprop(critic.parameters(), lr=config["lr"])
 
-    trainer = Trainer(generator, critic, optimG, optimC, seq_length)
+    plotmode = "bokeh"
+
+    trainer = Trainer(generator, critic, optimG, optimC, seq_length, plotmode=plotmode)
     trainer.train(dataloader, epochs=config["epochs"])
 
 
@@ -237,12 +318,12 @@ if __name__ == "__main__":
 
     NUM_TRIAL_RUNS = 4
     EXPERIMENT_NAME = "wgan-gp_experiment"
-    RESOURCES_PER_TRIAL = {"cpu": 2, "gpu": 2}
+    RESOURCES_PER_TRIAL = {"cpu": 1, "gpu": 4}
 
     config = {
         "lr": tune.choice([0.00005]),
-        "epochs": tune.choice([2000]),
-        "batch_size": tune.choice([10, 15, 20, 25]),
+        "epochs": tune.choice([1400]),
+        "batch_size": tune.choice([50]),
     }
 
     ray.init()
--- synthetic_data/mlops/vizulize.py
+++ synthetic_data/mlops/vizulize.py
@@ -1,36 +1,28 @@
-from bokeh.plotting import figure, show
+from matplotlib import pyplot as plt
 from torch.utils.data import DataLoader
 
 from synthetic_data.mlops.tmp.data import HarmonicDataset
 
-BATCH_SIZE = 10
-SPLIT_SIZE = 100
+BATCH_SIZE = 50
+SPLIT_SIZE = 50
 SPLIT_RATIO = 1
 DATASET_NAME = "AMP20"
 
 dataset = HarmonicDataset(DATASET_NAME, SPLIT_SIZE, SPLIT_RATIO)
 dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-N_SAMPLES = 5
+shapes = dataset.dataset.numpy().shape
+batches = len(iter(dataloader))
+print(f"Data  : {batches} x {shapes}")
 
-fig = figure(
-    title=f"Dataset: {DATASET_NAME}",
-    x_axis_label="Timesteps",
-    y_axis_label="Amplitude",
-    max_height=300,
-    height_policy="max",
-)
+for i, data in enumerate(dataloader):
+    print(f"Batch {i}: {data.numpy().shape}")
 
+plt.figure(figsize=(10, 4))
 for i, data in enumerate(dataloader):
     y = data[0].numpy()
     x = range(len(y))
-    if i < N_SAMPLES:
-        fig.line(x, y, legend_label="Regular")
-        fig.circle(
-            x=x,
-            y=y,
-            legend_label="Regular",
-            fill_color="blue",
-            size=1,
-        )
-show(fig)
+
+    plt.plot(x, y, label=f"Batch {i}")
+    break
+plt.show()
