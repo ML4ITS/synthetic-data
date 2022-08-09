5426250e1867994ca443ea64fe7b6d533f026f08
andreasoie <oeie.andreas@gmail.com>
2022-08-09 11:15:07 +0200

    refactor
    - moved out data fetch method
    - added transforms
    - changed saving/pathing

--- synthetic_data/mlops/datasets/multiharmonic.py
+++ synthetic_data/mlops/datasets/multiharmonic.py
@@ -9,61 +9,84 @@ from torch.utils.data import Dataset
 from synthetic_data.common.config import RemoteConfig
 from synthetic_data.mlops.tools.api import load_dataset
 
-RAY_DATAFOLDER = "/data/summer_internship22/cgan"
-os.makedirs(RAY_DATAFOLDER, exist_ok=True)
+
+def _fetch_dataset(
+    datset_names: List[str], label_map: Dict[str, int]
+) -> Tuple[torch.Tensor, torch.Tensor]:
+
+    all_labels = []
+    all_datasets = []
+    cfg = RemoteConfig()
+
+    # Assuming that the names are in the same order as the labels.
+    # Also, using chronological indices as 'labels' helps a lot for simplicity.
+    for datset_name, dataset_label in zip(datset_names, label_map.values()):
+        t1 = time.monotonic()
+        dataset = load_dataset(cfg, datset_name)
+        t2 = time.monotonic()
+        print(f"Downloaded {datset_name} in {t2 - t1:.3f} seconds")
+        assert dataset.ndim == 2, f"MultiHarmonicDataset should be 2-D"
+        labels = np.array([dataset_label] * dataset.shape[0])
+        all_datasets.append(dataset)
+        all_labels.append(labels)
+
+    all_labels = np.concatenate(all_labels, axis=0)
+    all_datasets = np.concatenate(all_datasets, axis=0)
+
+    return torch.from_numpy(all_datasets), torch.from_numpy(all_labels)
 
 
 class MultiHarmonicDataset(Dataset):
 
-    filename_data = os.path.join(RAY_DATAFOLDER, "data.pt")
-    filename_labels = os.path.join(RAY_DATAFOLDER, "labels.pt")
+    _name = "MultiHarmonicDataset"
 
-    def __init__(self, names: List[str], label_map: Dict[str, int]) -> None:
+    _COMPUTE_FOLDER = "/data/summer_internship22/cgan"
+    _path_to_dataset = os.path.join(_COMPUTE_FOLDER, _name)
+
+    def __init__(
+        self,
+        names: List[str],
+        label_map: Dict[str, int],
+        transforms: torch.nn.Module = None,
+        save: bool = False,
+        local_dir: str = None,
+    ) -> None:
         super().__init__()
         self.names = names
         self.label_map = label_map
+        self.transforms = transforms
 
-        if os.path.exists(self.filename_data) and os.path.exists(self.filename_labels):
-            print("Loading cached data ...")
-            self.data = torch.load(self.filename_data)
-            self.labels = torch.load(self.filename_labels)
+        if local_dir is not None:
+            self.savedir = os.path.join(local_dir, self._name)
         else:
-            print("Downloading data...")
-            self.data, self.labels = self._load_data()
-            torch.save(self.data, self.filename_data)
-            torch.save(self.labels, self.filename_labels)
+            self.savedir = os.path.join(self._COMPUTE_FOLDER, self._name)
+
+        _path_to_data = os.path.join(self.savedir, "data.pt")
+        _path_to_labels = os.path.join(self.savedir, "labels.pt")
+
+        if os.path.exists(_path_to_data) and os.path.exists(_path_to_labels):
+            print("Loading data ...")
+            self.data = torch.load(_path_to_data)
+            self.labels = torch.load(_path_to_labels)
+        else:
+            print("Fetching data...")
+            self.data, self.labels = _fetch_dataset(self.names, self.label_map)
+            if save:
+                os.makedirs(self.savedir, exist_ok=True)
+                torch.save(self.data, _path_to_data)
+                torch.save(self.labels, _path_to_labels)
+                print(f"Saved data to {self.savedir}")
 
     def __len__(self) -> int:
         return len(self.data)
 
     def __getitem__(self, index: int) -> Tuple[Any, Any]:
-        data, label = self.data[index], int(self.labels[index])
-        return data, label
+        batch = self.data[index], int(self.labels[index])
+
+        if self.transforms is not None:
+            batch = self.transforms(batch)
+
+        return batch
 
     def get_sequence_length(self) -> int:
         return self.data.shape[1]
-
-    def _load_data(self):
-        cfg = RemoteConfig()
-
-        all_labels = []
-        all_datasets = []
-        # Assuming that the names are in the same order as the labels.
-        # Also, using chronological indices as 'labels' helps a lot for simplicity.
-        for name, index in zip(self.names, self.label_map.values()):
-            t1 = time.monotonic()
-            dataset = load_dataset(cfg, name)
-            t2 = time.monotonic()
-            print(f"Loaded {name} in {t2 - t1:.3f} seconds")
-            assert dataset.ndim == 2, f"MultiHarmonicDataset should be 2-D"
-            labels = np.array([index] * dataset.shape[0])
-            all_datasets.append(dataset)
-            all_labels.append(labels)
-
-        all_labels = np.concatenate(all_labels, axis=0)
-        all_datasets = np.concatenate(all_datasets, axis=0)
-
-        all_datasets = torch.from_numpy(all_datasets)
-        all_labels = torch.from_numpy(all_labels)
-
-        return all_datasets, all_labels
