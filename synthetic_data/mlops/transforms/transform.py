39bf3f8ba6d1da923d12585f8f8644b9a52b62db
andreasoie <oeie.andreas@gmail.com>
2022-08-09 11:15:37 +0200

    added random roll
    (data augmentation)

---
+++ synthetic_data/mlops/transforms/transform.py
@@ -0,0 +1,29 @@
+from typing import Tuple
+
+import numpy as np
+import torch
+
+
+class RandomRoll(torch.nn.Module):
+    def __init__(self, p: float = 0.5) -> None:
+        super().__init__()
+        self.p = p
+
+    @torch.no_grad()
+    def forward(
+        self, sample: Tuple[torch.Tensor, torch.Tensor]
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+
+        sequence = sample[0]
+        target = sample[1]
+
+        if torch.rand(1).item() < self.p:
+            return sequence, target
+
+        _min = 0
+        _max = len(sequence)
+        _roll = torch.randint(_min, _max, (1,)).item()
+
+        sequence = torch.roll(sequence, _roll, dims=0)
+
+        return sequence, target
