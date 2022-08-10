0305c36a96f5afa4fa2e080393371d89c44161c7
andreasoie <oeie.andreas@gmail.com>
2022-08-10 17:58:07 +0200

    refactored c-GAN

--- synthetic_data/mlops/models/cgan.py
+++ synthetic_data/mlops/models/cgan.py
@@ -1,19 +1,18 @@
-# inspired by https://github.com/eriklindernoren/PyTorch-GAN
+"""
+Conditional GAN implementation
 
+inspired by https://github.com/eriklindernoren/PyTorch-GAN
+"""
 import torch
 from torch import nn
 
 
 class Generator(nn.Module):
-    def __init__(self, seq_length: int, n_classes: int, latent_dim: int) -> None:
+    def __init__(self, seq_length: int, n_classes: int, z_dim: int) -> None:
         super().__init__()
-        self.seq_length = seq_length
-        self.n_classes = n_classes
-        self.latent_dim = latent_dim
-
-        self.embedder = nn.Embedding(self.n_classes, self.n_classes)
-        input_channels = self.latent_dim + self.n_classes
-        output_channels = self.seq_length
+        self.embedder = nn.Embedding(n_classes, n_classes)
+        input_channels = z_dim + n_classes
+        output_channels = seq_length
 
         self.model = nn.Sequential(
             nn.Linear(input_channels, 128),
@@ -37,11 +36,8 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
     def __init__(self, seq_length: int, n_classes: int) -> None:
         super().__init__()
-        self.seq_length = seq_length
-        self.n_classes = n_classes
-
-        self.embedder = nn.Embedding(self.n_classes, self.n_classes)
-        input_channels = self.seq_length + self.n_classes
+        self.embedder = nn.Embedding(n_classes, n_classes)
+        input_channels = seq_length + n_classes
         output_channels = 1
 
         self.model = nn.Sequential(
@@ -58,29 +54,13 @@ class Discriminator(nn.Module):
 
     def forward(self, seq: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
         label_embedding = self.embedder(labels)
-        seq = torch.cat((seq, label_embedding), -1)
-        out = self.model(seq.float())
+        seq = torch.cat((seq, label_embedding), -1).float()
+        out = self.model(seq)
         return out.squeeze()
 
 
 if __name__ == "__main__":
 
-    N_CLASSES = 10
-    SEQ_LENGTH = 200
+    from synthetic_data.mlops.tools.summary import summarize_conditional_gan
 
-    BATCH_SIZE = 256
-    LATENT_DIM = 100
-
-    gen = Generator(SEQ_LENGTH, LATENT_DIM, N_CLASSES)
-    dis = Discriminator(SEQ_LENGTH, LATENT_DIM, N_CLASSES)
-
-    noise_shape = (BATCH_SIZE, LATENT_DIM)
-    noise = torch.randn(noise_shape)
-    rand_labels = torch.randint(0, N_CLASSES, (BATCH_SIZE,))
-
-    seq = gen(noise, rand_labels)
-    probs = dis(seq, rand_labels)
-
-    print("Noise :", noise.shape)
-    print("  Seq :", seq.shape)
-    print("Probs :", probs.shape)
+    summarize_conditional_gan(Generator, Discriminator, n_classes=10)
